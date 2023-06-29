265bd979a8b829b7e6ca5639fb627b2998c712c6
MlMo <25961416+mlmoravek@users.noreply.github.com>
2023-06-29 23:11:50 +0200

    skeleton: theme

--- src/components/Skeleton.vue
+++ src/components/Skeleton.vue
@@ -1,33 +1,55 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const animated = ref(true);
+</script>
+
 <template>
   <section>
+    <h2>Skeleton Demo</h2>
+    <hr />
+  </section>
+
+  <section class="py-4">
+    <h3>Base</h3>
     <o-field grouped group-multiline>
       <o-switch v-model="animated">Animated</o-switch>
     </o-field>
+    20%
+    <o-skeleton width="20%" :animated="animated" />
+    40%
+    <o-skeleton width="40%" :animated="animated" />
+    80%
+    <o-skeleton width="80%" :animated="animated" />
+    Count 2
+    <o-skeleton :animated="animated" :count="2" />
+  </section>
 
-    <o-skeleton width="20%" :animated="animated"></o-skeleton>
+  <section class="py-4">
+    <h3>Circle</h3>
 
-    <o-skeleton width="40%" :animated="animated"></o-skeleton>
+    <o-skeleton :animated="animated" circle width="64px" height="64px" />
+  </section>
 
-    <o-skeleton width="80%" :animated="animated"></o-skeleton>
+  <section class="py-4">
+    <h3>Sizes</h3>
+    Small
+    <o-skeleton :animated="animated" size="small" />
+    Default
+    <o-skeleton :animated="animated" />
+    Medium
+    <o-skeleton :animated="animated" size="medium" />
+    Large
+    <o-skeleton :animated="animated" size="large" />
+  </section>
 
-    <o-skeleton :animated="animated"></o-skeleton>
-
-    <o-skeleton
-      :animated="animated"
-      circle
-      width="64px"
-      height="64px"></o-skeleton>
-
-    <o-skeleton :animated="animated" size="large" :count="2"></o-skeleton>
+  <section class="py-4">
+    <h3>Position</h3>
+    Left
+    <o-skeleton width="30%" position="left" :animated="animated" />
+    Center
+    <o-skeleton width="30%" position="centered" :animated="animated" />
+    Right
+    <o-skeleton width="30%" position="right" :animated="animated" />
   </section>
 </template>
-
-<script lang="ts">
-export default {
-  data() {
-    return {
-      animated: true,
-    };
-  },
-};
-</script>
