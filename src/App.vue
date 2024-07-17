77ca1b728e335a9d6b99f2c3a2b0f7360815f154
mlmo <marcel.moravek@yahoo.de>
2024-07-17 11:32:29 +0200

    feat(oruga): update oruga version

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,29 @@
 <script setup lang="ts">
-const paths = import.meta.glob("./components/*.vue") as any;
-const components = Object.keys(paths)
-  .map((c: string) => c.replace(".vue", ""))
-  .map((c: string) => c.replace("./components/", ""))
-  .map((c: string) => `/${c}`)
-  .map((c: string) => {
-    const name = c.split("/")[1];
-    return {
-      name,
-      link: c,
-    };
-  });
+import components from "@/components";
 </script>
 
 <template>
   <div id="app">
-    <nav id="nav">
+    <aside id="nav">
       <router-link to="/" class="py-4 ps-0 text-center">
         <img
           class="image__oruga"
           src="https://raw.githubusercontent.com/oruga-ui/theme-bootstrap/master/public/logo.svg"
           alt="Logo Oruga Theme Bootstrap" />
-        <b class="fs-5 text-white">Oruga Bootstrap Theme</b>
+        <b>Oruga Bootstrap Theme</b>
       </router-link>
-      <hr />
-      <router-link v-for="item in components" :key="item.name" :to="item.link">
-        {{ item.name }}
-      </router-link>
-    </nav>
 
-    <main>
+      <hr />
+
+      <router-link
+        v-for="component in components"
+        :key="component"
+        :to="{ name: component }">
+        {{ component }}
+      </router-link>
+    </aside>
+
+    <main class="main">
       <router-view />
     </main>
   </div>
@@ -39,9 +33,13 @@ const components = Object.keys(paths)
 #app {
   display: flex;
   width: 100%;
+
   main {
     flex-grow: 1;
-    margin: 25px;
+    height: 100vh;
+    padding: 2rem;
+    padding-bottom: 6rem;
+    overflow-y: scroll;
 
     > section {
       padding: 1rem 0;
@@ -60,20 +58,33 @@ const components = Object.keys(paths)
   #nav {
     display: flex;
     flex-direction: column;
-    background-color: #7952b3;
-    height: auto;
-    min-height: 100vh;
-    width: 15%;
+    height: 100vh;
+    min-width: var(--vp-sidebar-width);
+    padding: 20px;
+    padding-top: 0px;
+    overflow-y: scroll;
+    background-color: var(--vp-sidebar-bg-color);
+
+    .theme-label {
+      font-size: 1.25rem;
+      text-align: center;
+      padding: 1rem 0;
+    }
+
+    hr {
+      width: 100%;
+      margin-top: 0;
+    }
 
     a {
-      font-weight: bold;
-      color: white;
+      font-weight: 500;
+      color: var(--vp-c-text-1);
       text-decoration: none;
-      padding: 1px 0 1px 10px;
+      padding: 5px;
+      font-size: 1rem;
 
       &.router-link-exact-active {
-        background-color: rgb(181, 122, 255);
-        color: white /*#2c3e50 /*#42b983*/;
+        color: rgb(181, 122, 255);
       }
     }
   }
