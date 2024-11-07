a60d97493f85d3b301929fa4039eeaf6b9e08601
mlmoravek <marcel.moravek@yahoo.de>
2024-11-07 16:58:01 +0100

    build: switch from rollup to vite build

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,23 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import dts from "vite-plugin-dts";
+import banner from "vite-plugin-banner";
 import { viteStaticCopy as copy } from "vite-plugin-static-copy";
 
 import { fileURLToPath } from "url";
 import { resolve } from "path";
+
+import pkg from "./package.json" with { type: "json" };
+
+function generate(version: string): string {
+  const preview_build = process.env.ORUGA_PREVIEW_BUILD;
+
+  if (preview_build) {
+    version = `${version} (build ${preview_build})`;
+  }
+
+  return `/*! Oruga Bootstrap Theme v${version} | MIT License | github.com/oruga-ui/theme-bootstrap */`;
+}
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -71,6 +84,8 @@ export default defineConfig(({ mode }) => {
             },
           ],
         }),
+        // adds a banner to every generated dist file
+        banner(generate(pkg.version)),
       ],
     };
   }
