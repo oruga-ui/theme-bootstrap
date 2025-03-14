47d53269a5e1a25c0ac7345bf6b71bf355f39874
mlmoravek <marcel.moravek@yahoo.de>
2025-03-14 11:07:06 +0100

    chore: readd theme styles to dev build

--- src/main.ts
+++ src/main.ts
@@ -12,22 +12,25 @@ import "@oruga-ui/examples/dist/examples.css";
 // add theme config
 import { bootstrapConfig } from "./plugins/theme";
 
+// add theme styles
+import "./assets/scss/bootstrap-build.scss";
+
 createApp(App)
-      .use(router)
-      .use(Oruga, {
-            iconPack: "fas",
-            iconComponent: "vue-fontawesome",
-            customIconPacks: {
-                  fas: {
-                        sizes: {
-                              default: "",
-                              small: "fa-sm",
-                              medium: "fa-lg",
-                              large: "fa-xl",
-                        },
-                  },
+    .use(router)
+    .use(Oruga, {
+        iconPack: "fas",
+        iconComponent: "vue-fontawesome",
+        customIconPacks: {
+            fas: {
+                sizes: {
+                    default: "",
+                    small: "fa-sm",
+                    medium: "fa-lg",
+                    large: "fa-xl",
+                },
             },
-            ...bootstrapConfig,
-      })
-      .use(Examples)
-      .mount("#app");
+        },
+        ...bootstrapConfig,
+    })
+    .use(Examples)
+    .mount("#app");
